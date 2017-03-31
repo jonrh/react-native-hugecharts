8c5cfc037bcd7a47f2e82f371d70844027b9f26c
Jón Rúnar Helgason <dev@jonrh.is>
2017-03-31 18:02:40 +0100

    Reformatted react-native-highcharts.js with Prettier, default arguments

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -1,20 +1,21 @@
-import React, { Component, PropTypes, } from 'react';
+import React, { Component, PropTypes } from "react";
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    WebView,
-    Image,
-    Dimensions
-} from 'react-native';
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  WebView,
+  Image,
+  Dimensions
+} from "react-native";
 
-const win = Dimensions.get('window');
+const win = Dimensions.get("window");
 class ChartWeb extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state={
+    // prettier-ignore
+    this.state={
             init:`<html>
                     <style media="screen" type="text/css">
                     #container {
@@ -49,75 +50,75 @@ class ChartWeb extends Component {
                 width:win.width
             }
         }
-    }
+  }
 
-    // used to resize on orientation of display
-    reRenderWebView(e) {
-        this.setState({
-            height: e.nativeEvent.layout.height,
-            width: e.nativeEvent.layout.width,
-        })
-    }
+  // used to resize on orientation of display
+  reRenderWebView(e) {
+    this.setState({
+      height: e.nativeEvent.layout.height,
+      width: e.nativeEvent.layout.width
+    });
+  }
 
-    render() {
-        let config = JSON.stringify(this.props.config, function (key, value) {//create string of json but if it detects function it uses toString()
-            return (typeof value === 'function') ? value.toString() : value;
-        });
+  render() {
+    let config = JSON.stringify(this.props.config, function(key, value) {
+      //create string of json but if it detects function it uses toString()
+      return typeof value === "function" ? value.toString() : value;
+    });
 
+    config = JSON.parse(config);
+    let concatHTML = `${this.state.init}${flattenObject(config)}${this.state.end}`;
 
-        config = JSON.parse(config)
-        let concatHTML = `${this.state.init}${flattenObject(config)}${this.state.end}`;
+    return (
+      <View style={this.props.style}>
+        <WebView
+          onLayout={this.reRenderWebView}
+          style={styles.full}
+          source={{ html: concatHTML, baseUrl: "web/" }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          scalesPageToFit={true}
+          scrollEnabled={false}
+          automaticallyAdjustContentInsets={true}
+        />
+      </View>
+    );
+  }
+}
 
-        return (
-          <View style={this.props.style}>
-              <WebView
-                  onLayout={this.reRenderWebView}
-                  style={styles.full}
-                  source={{ html: concatHTML, baseUrl: 'web/' }}
-                  javaScriptEnabled={true}
-                  domStorageEnabled={true}
-                  scalesPageToFit={true}
-                  scrollEnabled={false}
-                  automaticallyAdjustContentInsets={true}
-              />
-          </View>
-        );
-    };
-};
-
-var flattenObject = function (obj, str='{') {
-    Object.keys(obj).forEach(function(key) {
-        str += `${key}: ${flattenText(obj[key])}, `
-    })
-    return `${str.slice(0, str.length - 2)}}`
+var flattenObject = function(obj, str = "{") {
+  Object.keys(obj).forEach(function(key) {
+    str += `${key}: ${flattenText(obj[key])}, `;
+  });
+  return `${str.slice(0, str.length - 2)}}`;
 };
 
 var flattenText = function(item) {
-    var str = ''
-    if (item && typeof item === 'object' && item.length == undefined) {
-        str += flattenObject(item)
-    } else if (item && typeof item === 'object' && item.length !== undefined) {
-        str += '['
-        item.forEach(function(k2) {
-            str += `${flattenText(k2)}, `
-        })
-        str = str.slice(0, str.length - 2)
-        str += ']'
-    } else if(typeof item === 'string' && item.slice(0, 8) === 'function') {
-        str += `${item}`
-    } else if(typeof item === 'string') {
-        str += `\"${item.replace(/"/g, '\\"')}\"`
-    } else {
-        str += `${item}`
-    }
-    return str
+  var str = "";
+  if (item && typeof item === "object" && item.length == undefined) {
+    str += flattenObject(item);
+  } else if (item && typeof item === "object" && item.length !== undefined) {
+    str += "[";
+    item.forEach(function(k2) {
+      str += `${flattenText(k2)}, `;
+    });
+    str = str.slice(0, str.length - 2);
+    str += "]";
+  } else if (typeof item === "string" && item.slice(0, 8) === "function") {
+    str += `${item}`;
+  } else if (typeof item === "string") {
+    str += `\"${item.replace(/"/g, '\\"')}\"`;
+  } else {
+    str += `${item}`;
+  }
+  return str;
 };
 
 var styles = StyleSheet.create({
-    full: {
-        flex:1,
-        backgroundColor:'transparent'
-    }
+  full: {
+    flex: 1,
+    backgroundColor: "transparent"
+  }
 });
 
 module.exports = ChartWeb;
